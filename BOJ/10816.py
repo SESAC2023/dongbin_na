a30eac15d52c40b2603ffe31e4f9773c44b39a0a
ndb7967 <122754860+ndb7967@users.noreply.github.com>
2023-06-28 22:27:19 +0900

    Update 10816.py

--- BOJ/10816.py
+++ BOJ/10816.py
@@ -31,3 +31,42 @@ for query in queries:
         print(counter[query], end=" ")
     else: # 한 번도 등장하지 않았다면
         print(0, end=" ")
+
+
+""" Solution 2
+
+데이터의 개수 N: 최대 50만
+쿼리의 개수 M: 최대 50만
+
+1) 데이터 입력 후에 정렬: O(NlogN) => 시간 초과 X
+2) 각 쿼리마다 이진 탐색 수행: O(MlogN) => 시간 초과 X
+
+즉, 최종 시간 복잡도: O(NlogN + MlogN)
+"""
+"""
+from bisect import bisect_left, bisect_right
+
+
+# 값이 [left_value, right_value]인 데이터의 개수를 반환하는 함수
+def count_by_range(a, left_value, right_value):
+    right_index = bisect_right(a, right_value)
+    left_index = bisect_left(a, left_value)
+    return right_index - left_index
+
+
+n = int(input())  # 데이터의 개수 N
+# 전체 데이터 입력받기
+arr = list(map(int, input().split()))
+
+m = int(input())  # 쿼리의 수 M
+# 개수를 세고자하는 쿼리 데이터들
+queries = list(map(int, input().split()))
+
+# 이진 탐색을 위해 데이터 정렬이 필요
+arr.sort()  # O(NlogN)
+
+# 각 쿼리마다 이진 탐색 수행: O(MlogN)
+for query in queries:
+    cnt = count_by_range(arr, query, query)
+    print(cnt, end=" ")
+"""
